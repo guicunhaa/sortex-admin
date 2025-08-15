eee40bdce1ab7f4ce8bf69496fbf3a484d2ea37b
guicunhaa <gui20101950jb@gmail.com>
2025-08-14 21:59:22 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  // Não deixar o build falhar por lint/TS em produção
+  // não falhar build por lint/type em produção
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 }
