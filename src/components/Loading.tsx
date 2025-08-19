23c3c722ff4f28a8260fabbd96dc516ee4c83613
guicunhaa <gui20101950jb@gmail.com>
2025-08-19 02:44:58 -0300

    Delete src directory

--- src/components/Loading.tsx
+++
@@ -1,8 +0,0 @@
-// components/Loading.tsx
-export default function Loading() {
-  return (
-    <div className="w-full h-40 flex items-center justify-center">
-      <div className="animate-spin rounded-full h-8 w-8 border-4 border-indigo-600 border-t-transparent"/>
-    </div>
-  );
-}
