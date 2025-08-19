2e19f6d6c8c0e964d0cf129b7a57724cc01b620a
guicunhaa <gui20101950jb@gmail.com>
2025-08-19 03:04:42 -0300

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
-  // não falhar build por lint/type em produção
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-}
-
-export default nextConfig
